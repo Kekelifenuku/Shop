dbe28decb6ee4de8ecd26b39a6fc169b8a03af24
Fenuku kekeli <113697135+Kekelifenuku@users.noreply.github.com>
2022-11-25 00:02:54 +0000

    Add files via upload

---
+++ SweaterShopApp-main/SweaterShopApp/SweaterShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopAppApp.swift
+//  SweaterShopApp
+//
+//  Created by Kekeli on 2022-11-23.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
